c81f8f40a078a7e0e4bf43388a6cdc2c47cc12fe
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-10 10:37:16 +0300

    Added meetup list on main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-const MainPage = () => {
-  return <div> Main page </div>;
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
-export default MainPage;
+export default HomePage;
