6b6695429eee7a2b1f963e68022af11339a7c143
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-11 20:31:21 +0300

    Connect mongoDb atlas cluster to the app and handling post request

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,26 @@
+import { MongoClient } from "mongodb";
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { address, description, image, title } = data;
+    // const { address, description, image, title } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://mnelyubin:1804@cluster0.yrunn.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup has been inserted" });
   }
 }
 
