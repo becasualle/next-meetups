370599a474f88fe12cb8cf884fcde06cec3785cd
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-05-12 10:01:17 +0300

    Sending Http Requests To Our API Routes

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,25 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const onAddMeetupHandler = (meetupData) => console.log(meetupData);
+  const router = useRouter();
+
+  async function onAddMeetupHandler(meetupData) {
+    // instead external url send request to the same server but different path
+    // will trigger handler function in pages/api/new-meetup.js
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+    router.push("/");
+  }
   return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
 };
 
